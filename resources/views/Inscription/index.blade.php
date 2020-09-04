616c7c9e9f412b38bf29ad92a4dc8b391d739e38
Christian <christianaka65@gmail.com>
2020-09-04 14:28:05 +0000

    page_inscription

---
+++ resources/views/Inscription/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('pages')
+    <!--Header End-->
+    <!--Inner Header Start-->
+    <section class="wf100 p100 inner-header">
+        <div class="container">
+            <h1>INSCRIPTION</h1>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Page inscription</a></li>
+            </ul>
+        </div>
+    </section>
+    <!--Inner Header End-->
+    <!--Causes Start-->
+    <section class="wf100 p80">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--Causes End-->
+
+@endsection
