ea9efc15acb634b2381800be8c1cf868543d9dde
Christian <christianaka65@gmail.com>
2020-10-12 11:07:43 +0000

    maj3.0

--- app/Http/Controllers/Admin/AdminControllers.php
+++ app/Http/Controllers/Admin/AdminControllers.php
@@ -14,6 +14,7 @@ class AdminControllers extends Controller
         $result = DB::table('dossier_inscriptions')
             ->join('candidats','dossier_inscriptions.id_cand','=','candidats.id_cand')
             ->join('etats','dossier_inscriptions.validation','=','etats.id_table')
+            ->orderBy('dossier','ASC')
             ->get();
 
         $nbre = DB::table('dossier_inscriptions')->count('dossier');
